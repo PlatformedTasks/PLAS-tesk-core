21b1e66d70652b1f3cffb3b1348f294bdc8074aa
ludovico.funari <ludovico.funari@gmail.com>
2022-12-07 14:38:36 +0000

    Add pvc to helm

--- src/tesk_core/helm_client.py
+++ src/tesk_core/helm_client.py
@@ -17,7 +17,22 @@ def helm_add_repo(repo_url):
         print(err)
 
 
-def helm_install(release_name, chart_name, chart_version, chart_values, task_name, namespace="default"):
+def pvc_to_helm(pvc):
+    helm_set = list()
+
+    for position, volume_mount in enumerate(pvc.volume_mounts):
+        for volume_mount_key in volume_mount:
+            helm_set.append(
+                f"--set volumeMounts[{position}].{volume_mount_key}={pvc.volume_mounts[volume_mount_key]}")
+
+    helm_set.append(
+        f"--set volumes[0].name={pvc.volume_mounts[0]['name']} --set volumes[0].persistentVolumeClaim.claimName={pvc.spec['metadata']['name']}")
+
+    setHelmVolumes = ' '.join(helm_set)
+    return setHelmVolumes
+
+
+def helm_install(release_name, chart_name, chart_version, chart_values, task_name, namespace="default", pvc=None):
     try:
         chart = f"{repo_name}/{chart_name}"
         print(f"Installing '{release_name}' from '{chart}' in namespace '{namespace}'...")
@@ -28,8 +43,6 @@ def helm_install(release_name, chart_name, chart_version, chart_values, task_nam
             release_name, 
             chart, 
             f'--namespace={namespace}', 
-            '--set',
-            f'transferVolume.pvc={task_name}-pvc',
             '--wait']
 
         if chart_version:
@@ -37,12 +50,17 @@ def helm_install(release_name, chart_name, chart_version, chart_values, task_nam
         if chart_values:
             for chart_file in chart_values:
                 helm_command.append(f'-f={str(chart_file)}')
+        if pvc:
+            helm_command.append(pvc_to_helm)
 
         release_install = subprocess.run(helm_command, capture_output=True, text=True, check=True)
         print(release_install.stdout)
+        
         return release_install
+
     except subprocess.CalledProcessError as err:
         print(err.stderr)
+
     except Exception as err:
         print(err)
 
@@ -52,7 +70,9 @@ def helm_uninstall(release_name, namespace="default"):
         print(f"Uninstalling '{release_name}'...")
         release_uninstall = subprocess.run(['helm', 'uninstall', release_name, f'--namespace={namespace}'], capture_output=True, text=True, check=True)
         print(release_uninstall.stdout)
+
     except subprocess.CalledProcessError as err:
         print(err.stderr)
+
     except Exception as err:
         print(err)
